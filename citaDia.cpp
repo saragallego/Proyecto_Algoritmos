d4e40f71e9056564ee1b44a4585f0a8503b45a45
saragallego <55637918+saragallego@users.noreply.github.com>
2019-11-24 21:36:02 -0500

    Delete citaDia.cpp

--- citaDia.cpp
+++
@@ -1,6 +0,0 @@
-#include "citaDia.hpp"
-
-citaDia::citaDia(bool d, Paciente k){
-    disponibilidad = d;
-    myPaciente = k;
-}//se inicializa el objeto cita dia con los valores dados como argumentos 
